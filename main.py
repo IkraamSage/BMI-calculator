249d0e531b538d14ed09ecb50d85c28349afaa9f
IkraamSage <82159115+IkraamSage@users.noreply.github.com>
2021-05-18 13:27:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
-
+# importing tkinter
 from tkinter import *
 from tkinter import messagebox
-
+# GUI size and headers
 root = Tk()
 root.title("BMI Calculator")
 root.geometry("750x660")
 root.config(bg="blue")
 header = Label(root, text='IDEAL BODY MASS INDEX CALCULATOR', fg='black')
 header.place(x=250, y=20)
-
+# GUI frame placement and size
 frame = Frame(root, width=400, height=200, relief='raised', bg='white')
 frame.place(x=190, y=80)
-
+# weight label and entry
 weight = Label(frame, text="Weight in kilograms:", bg='red', fg='black')
 weight.place(x=20, y=20)
 weight_entry = Entry(frame)
 weight_entry.place(x=180, y=20)
-
-height = Label(frame, text="Height in cetimeters:", bg='red', fg='black')
+# height label and entry
+height = Label(frame, text="Height in centimeters:", bg='red', fg='black')
 height.place(x=20, y=60)
 height_entry = Entry(frame)
 height_entry.place(x=180, y=60)
-
+# gender entry and placement
 gender = Label(frame, text="Gender:", bg='red', fg='black')
 gender.place(x=100, y=100)
-
+# age entry and placement
 age = Label(frame, text="Age:", bg='red', fg='black')
 age.place(x=120, y=160)
 age_entry = Entry(frame, state='readonly')
-age_entry.place(rely=0.8, relx=0.4)
-
+age_entry.place(x=180, y=160)
+# setting variable option for male and female selection
 options = ['Select...', 'Male', "Female"]
 variable = StringVar(frame)
 variable.set(options[0])
 
-
+# defining the value
 def activate(value):
     variable.set(value)
     if value != "Select...":
@@ -42,11 +42,11 @@ def activate(value):
     else:
         age_entry.config(state='readonly')
 
+# gender menu placement
+gender = OptionMenu(frame, variable, *options, command=activate)
+gender.place(x=180,  y=100)
 
-gender_menu = OptionMenu(frame, variable, *options, command=activate)
-gender_menu.place(relx=0.4, rely=0.5)
-
-
+# BMI calculation entry and results
 def bmi_calc():
     try:
         float(weight_entry.get())
@@ -88,10 +88,10 @@ def bmi_calc():
         messagebox.showerror(title=None, message='Gender was not specified or invalid entry was given')
         delete()
 
-
+# button for ideal body mass index
 calculate = Button(root, text="Calculate your Ideal Body Mass Index", width=50, command=bmi_calc)
-calculate.place(rely=0.45, relx=0.2)
-
+calculate.place(x=180, y=300)
+# making the size and placement of button
 bmi = Label(root, text="BMI:", bg='#333', fg="white")
 bmi.place(rely=0.55, relx=0.1)
 bmi_field = Entry(root, state='readonly')
@@ -101,7 +101,7 @@ ideal_bmi.place(rely=0.55, relx=0.5)
 ideal_field = Entry(root, state='readonly')
 ideal_field.place(rely=0.55, relx=0.65)
 
-
+#defining the delete button
 def delete():
     weight_entry.delete(0, END)
     height_entry.delete(0, END)
